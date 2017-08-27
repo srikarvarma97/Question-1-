dd9093e6c396ad2db01ffbc935d1d11d2e0d8fcf
srikarvarma97 <31039710+srikarvarma97@users.noreply.github.com>
2017-08-27 22:11:28 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.buddh.contacts;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    DownloadManager downloadManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                Uri uri = Uri.parse("http://www.cs.columbia.edu/~coms6998-8/assignments/homework2/contacts/contacts.txt");
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                Long reference = downloadManager.enqueue(request);
+            }
+
+        });
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMain3Activity();
+            }
+        });
+
+
+    }
+
+    private void goToMain3Activity() {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+}
